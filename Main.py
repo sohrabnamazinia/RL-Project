3fe4cda8b2bec17de78ef4e9a833655dce3d1de1
Sohrab Namazinia <sohrabnamazinia@laptop-f61c007l.resource.campus.njit.edu>
2023-03-21 21:09:18 -0400

    add import-policy

--- Main.py
+++ Main.py
@@ -1,7 +1,8 @@
 from Model import Model
 from Environment import Environment
+from Environment import Policy
 
-env = Environment()
+env = Environment(policy=Policy.CLOSENESS)
 model = Model(env)
 model.environment.print_grid()
 train_result = model.train()
